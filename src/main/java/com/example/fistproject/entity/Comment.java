faf0f303d21c72897a83cec67d48748efd95661c
kangwook-kim02 <cocacolra741@naver.com>
2025-08-26 01:02:02 +0900

    댓글 수정 api, 삭제 api 추가

--- src/main/java/com/example/fistproject/entity/Comment.java
+++ src/main/java/com/example/fistproject/entity/Comment.java
@@ -36,4 +36,15 @@ public class Comment {
         // 엔터티 생성 및 반환
         return new Comment(dto.getId(), article, dto.getNickname(), dto.getBody());
     }
+
+    public void patch(CommentDto dto) {
+        // 예외 발생
+        if (this.id != dto.getId())
+            throw new IllegalArgumentException("댓글 수정 실패! 잘못된 id가 입력됐습니다.");
+        // 객체 갱신
+        if(dto.getNickname()!=null) // nickname이 수정되었을 경우
+            this.nickname = dto.getNickname();
+        if(dto.getBody()!=null) // body가 수정되었을 경우
+            this.body = dto.getBody();
+    }
 }
